31d8d5935c32dbd71b40462c5461e9158057be0b
박민호 <minho@bagminhoui-MacBook-Pro.local>
2017-10-05 15:17:37 +0900

    숫자의 표현

---
+++ Level4__숫자의 표현/app.js
@@ -0,0 +1,20 @@
+function expressions(num) {
+    let answer = 0;
+    let temp = 0;
+
+    Loop:
+        for(let i=1; i<=num; i++){
+            for(let j=i; j<=num; j++){
+                temp += j;
+                if(temp === num) {
+                    answer += 1;
+                    continue Loop;
+                }
+            }
+            temp = 0;
+        }
+    return answer;
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log(expressions(15));
