63542ca45e69a97bb860b036cde5ae71274444be
minho <radlohead@naver.com>
2017-10-07 16:35:31 +0900

    변수 타입 추가

--- Custom__문자열 순서 뒤집기/app.ts
+++ Custom__문자열 순서 뒤집기/app.ts
@@ -1,7 +1,7 @@
 // 문자열 순서 뒤집기
 
 const stringTurn: Function = (str: string): any => {
-    let result = '';
+    let result: string = '';
     for(let i=str.length-1; i>=0; i--) {
         result += str[i];
     }
