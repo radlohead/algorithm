d30998928de2d8848aa5f57dba2b0d22bd275495
minho <radlohead@naver.com>
2017-10-10 23:34:57 +0900

    Lv5 줄 서는 방법 시작

--- Level5__2 x n 타일링/app.js
+++ Level5__2 x n 타일링/app.js
@@ -2,10 +2,11 @@ var tiling = function (n) {
     var result = 0;
     var arr = [1, 1];
     for (var i = 2; i <= n; i++) {
+        console.log(arr[i - 1] + arr[i - 2], arr[i - 1], arr[i - 2]);
         arr[i] = (arr[i - 1] + arr[i - 2]) % 100000;
     }
     result = arr[arr.length - 1];
     return result;
 };
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
-console.log(tiling(524)); //3650
+console.log(tiling(5)); //3650
--- Level5__2 x n 타일링/app.ts
+++ Level5__2 x n 타일링/app.ts
@@ -2,10 +2,11 @@ const tiling: Function = (n: number): number => {
     let result: number = 0;
     let arr: Array<number> = [1, 1];
     for(let i: number=2; i<=n; i++){
+        console.log(arr[i-1] + arr[i-2], arr[i-1], arr[i-2]);
         arr[i] = (arr[i-1] + arr[i-2]) % 100000;
     }
     result = arr[arr.length-1];
     return result;
 };
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
-console.log(tiling(524));  //3650
+console.log(tiling(5));  //3650
