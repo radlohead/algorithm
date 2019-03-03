09feb8cea0f3920cb837acd3b990228e3669a114
박민호 <radlohead@naver.com>
2019-03-03 22:47:32 +0900

    ZigZag Conversion 리팩토링

--- ZigZag Conversion/app.js
+++ ZigZag Conversion/app.js
@@ -30,36 +30,42 @@
 // 0,1,2,3
 
 const solution = (str, numRows) => {
-    const strLength = str.slice().length;
-    let result = [];
+    let result = '';
     let arr = [];
-    let tempStr = '';
+    const tempArr = Array(numRows)
+                        .fill(numRows)
+                        .map((v, i) => v - i)
+                        .splice(0, numRows - 1);
 
-    for(let i=0; i<strLength; i++){
-        for(let j=numRows; j>1; j--) {
-            if(!str.length) break;
-            if(j === numRows) {
-                tempStr = str.substr(0, j);
-                str = str.substr(j, str.length);
-                arr.push(tempStr.split(''));
-            } else {
-                tempStr = str.substr(0, 1);
-                str = str.substr(1, str.length);
-                
-                let tempArr = Array(4).fill('');
-                tempArr[j-1] = tempStr;
-                arr.push(tempArr);
-            }
-        }
+    const arrFull = (i, tempArr) => {
+        result = str.substr(0, tempArr[i]);
+        str = str.substr(tempArr[i], str.length);
+        arr.push(result.split(''));
     }
 
-    arr.forEach((v, i) => {
-        arr.forEach((a, j) => {
-            if(arr[j][i]) result.push(arr[j][i]);
+    const arrOne = (i, tempArr) => {
+        let arrFill = Array(numRows).fill('');
+
+        result = str.substr(0, 1);
+        str = str.substr(1, str.length);
+        arrFill[tempArr[i] - 1] = result;
+        arr.push(arrFill);
+    }
+    
+    Array(str.length).fill('').forEach(() => {
+        tempArr.forEach((v, i) => {
+            if(!str.length) return;
+            if(tempArr[i] === numRows) arrFull(i, tempArr); 
+            arrOne(i, tempArr);
         });
     });
     
-    return result.join('');
+    result = arr
+                .map((v, i) => arr.map((a, j) => arr[j][i]))
+                .join('')
+                .replace(/,/g, '');
+    
+    return result;
 }
 
 solution('PAYPALISHIRING', 4);  // PINALSIGYAHRPI
