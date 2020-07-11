9ea7b02ce1ba694da71f408ca97251ff30fe4660
radlohead <radlohead@naver.com>
2020-07-11 20:37:40 +0900

    Container With Most Water - 성능 개선

--- Container With Most Water/app.js
+++ Container With Most Water/app.js
@@ -1,7 +1,7 @@
-// Given n non-negative integers a1, a2, ..., an , where each 
-// represents a point at coordinate (i, ai). n vertical lines are drawn such 
-// that the two endpoints of line i is at (i, ai) and (i, 0). 
-// Find two lines, which together with x-axis forms a container, 
+// Given n non-negative integers a1, a2, ..., an , where each
+// represents a point at coordinate (i, ai). n vertical lines are drawn such
+// that the two endpoints of line i is at (i, ai) and (i, 0).
+// Find two lines, which together with x-axis forms a container,
 // such that the container contains the most water.
 
 // Note: You may not slant the container and n is at least 2.
@@ -11,20 +11,19 @@
 // Input: [1,8,6,2,5,4,8,3,7]
 // Output: 49
 
-const solution = (arg) => {
-    let result = 0;
-    for(let i=0; i<arg.length; i++){
-        for(let j=0; j<arg.length; j++){
-            for(let k=0; k<=j; k++){
-                if(j+i < arg.length && j !== 0) {
-                    const minSquareWater = Math.min(arg[i], arg[i+j]) * j;
-                    result < minSquareWater ? result = minSquareWater : result;
-                    break;
-                }
-            }
-        }
+const maxArea = (height) => {
+    let i = 0
+    let j = height.length - 1
+    let max = 0
+    let area
+
+    while (i < j) {
+        area = (j - i) * Math.min(height[i], height[j])
+        max = Math.max(area, max)
+
+        height[i] < height[j] ? ++i : --j
     }
-    return result;
+    return max
 }
 
-solution([1,8,6,2,5,4,8,3,7]);
+maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
